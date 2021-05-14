a174ad4e8129d05e7fe91645d138a008af77a908
ryfray <48455524+ryfray@users.noreply.github.com>
2021-05-14 07:13:28 -0600

    Server setup.

--- server.js
+++ server.js
@@ -24,3 +24,12 @@ app.use(express.static('website'));
 
 
 // Setup Server
+
+const port = 8000;
+const server = app.listen(port, listening);
+
+function listening () {
+
+    console.log('server is running');
+    console.log(`running on local host: ${port}`);
+};
