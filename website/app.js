809826eba31fb4f313a8837cf8a6529e5fef53b5
Ryan F <ryanfrayser@gmail.com>
2021-06-04 10:34:38 -0600

    Import WIndows Node Modules.
    Create successful API response.

--- website/app.js
+++ website/app.js
@@ -1,16 +1,16 @@
 /* Global Variables */
 
-
 // Open Weather API
 const apiKey = "8a1815c4bb8292c0b1d39a5094817e19";
-const apiLink = "api.openweathermap.org/data/2.5/weather?zip="
-
-
+const apiLink = "http://api.openweathermap.org/data/2.5/weather?zip="
 
 
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+
+
+
 
 
 // "Generate" Button Action
@@ -22,27 +22,22 @@ function createEntry (e){
     //Get Zipcode Entry
     const zipCode = document.getElementById('zip').value;
 
-    getWeather(apiLink, zipCode, apiKey)
+    getWeather(apiLink, zipCode, apiKey);
 };
-
-
-// //Full Weather Request API URL
-// const weatherRequestUrl = `${apiLink}${zipCode}&appid=${apiKey}`
-
 
 //Using the provided information, make a request to the API
 
 const getWeather = async (apiLink,  zipCode, apiKey) => {
 
-    const response = await fetch(apiLink+zipCode+apiKey)
+    //Make request to Open Weather API using the entered ZIP Code
+    const response = await fetch(`${apiLink}${zipCode}&appid=${apiKey}`)
 
     try {
-
         const data = await response.json();
-        console.log(data);
 
+        //Print the temperature returned for the location
+        console.log(data.main.temp);
     }
-
     catch (error) {
         console.log ( 'Something happened...', error)
     }
