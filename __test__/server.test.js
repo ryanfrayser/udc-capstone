7f9a6e25d207a40304b4f242ea76c6ce0de91109
ryfray <48455524+ryfray@users.noreply.github.com>
2021-08-27 12:36:27 -0600

    Fix express server testing.

--- __test__/server.test.js
+++ __test__/server.test.js
@@ -1,10 +1,8 @@
-//Import file
-
-const app = require('../src/server/server');
+const app = require('../src/server/server.js');
 const supertest = require ('supertest');
 const request = supertest(app)
 
-it('gets the test endpoint', async done => {
+it('gets the test endpoint', async () => {
     const response = await request.get('/test')
 
     expect(response.status).toBe(200);
